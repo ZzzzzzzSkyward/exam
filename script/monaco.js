ecf78104abc98895ecb220fbb62e87d3f5a918a3
ZzzzzzzSkyward <zhu.zheng.sheng@163.com>
2021-10-11 23:37:42 +0800

    fixed a bug when index.html didnt display properly

--- script/monaco.js
+++ script/monaco.js
@@ -1,3 +1,37 @@
+function translateMonacoKey(keys) {
+    if (typeof keys === "string") keys = [keys];
+    let result = 0,
+        t = 0;
+    for (let key of keys) {
+        key = key.toLowerCase();
+        let k = monaco.KeyMod;
+        let dict = {
+            control: k.CtrlCmd,
+            ctrl: k.CtrlCmd,
+            alt: k.AltCmd,
+            shift: k.Shift,
+            '[': k.US_OPEN_SQUARE_BRACKET,
+            '=': k.US_EQUAL,
+            '-': k.US_MINUS,
+
+        }
+        if (dict.hasOwnProperty(key)) t = dict[key];
+        else t = k[key];
+        result |= t;
+    }
+    return result;
+}
+
+function translateMonacoAction(action) {
+    switch (action) {
+        case 'unfoldAll':
+            action = "unfondRecursively";
+            break;
+    }
+    action = "editor." + action;
+    return action;
+}
+
 function startMonaco(input, parent, id, options) {
     if (!id) id = "vscode";
     var v = zzz.create("div", {
@@ -12,8 +46,26 @@ function startMonaco(input, parent, id, options) {
             roundedSelection: false,
             scrollBeyondLastLine: false,
             readOnly: false,
+            automaticLayout: true,
+            lineNumbers: true,
+            glyphMargin: true,
+            //fontFamily:"",
+            minimap: {
+                enabled: true
+            },
+            lightbulb: {
+                enabled: true
+            },
             fontSize: zzz.browser.screenX / 100 * 1.5 + "px"
         });
+        window[id].bindKey = function (key, action) {
+            key = translateMonacoKey(key);
+            action = translateMonacoAction(action);
+            editor.addCommand(key, function () {
+                editor.trigger('', action);
+            });
+        }
+
         if (options) window[id].updateOptions(options);
         monaco.editor.defineTheme('anotherTheme', {
             base: 'vs',
@@ -43,9 +95,9 @@ function startBrython() {
         console.out = function () {
             var t = "";
             for (let i of arguments) t += i;
-            vscodeConsole.setValue(vscodeConsole.getValue()+t);
+            vscodeConsole.setValue(vscodeConsole.getValue() + t);
         }
-        __BRYTHON__.stdout.out="out";
+        __BRYTHON__.stdout.out = "out";
         zzz.incidence.bind(zzz.get.cls("answer")[0].firstElementChild, "dblclick", (e) => {
             zzz.get.id("runner").innerHTML = vscode.getValue();
             brython({
